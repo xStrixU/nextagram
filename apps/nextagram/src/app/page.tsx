1d67c9b62d8a5fae015bb7f755cea955a177d78c
Kacper Polak <strixu155@gmail.com>
2025-05-26 11:39:02 +0200

    feat: add shadcn

--- apps/nextagram/src/app/page.tsx
+++ apps/nextagram/src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button, Input } from '@nextagram/nextagram-shared-ui';
+import { cn } from '@nextagram/nextagram-shared-utils-styling';
+
 const IndexPage = () => (
-	<>
+	<main className="flex h-screen flex-col items-center justify-center gap-6">
 		<h1 className="text-4xl font-bold text-blue-500">Welcome in Nextagram!</h1>
-	</>
+		<Button>Lorem Ipsum</Button>
+		<Input placeholder="Lorem ipsum" className="max-w-xl" />
+		<p className={cn('text-red-300')}>Foo</p>
+	</main>
 );
 
 export default IndexPage;
