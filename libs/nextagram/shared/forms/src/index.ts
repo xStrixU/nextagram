f997a5fa6fea4c0dd39341b2242d97edfb27b0b6
Kacper Polak <strixu155@gmail.com>
2025-06-02 10:24:06 +0200

    refactor: refactor form-item-wrapper

--- libs/nextagram/shared/forms/src/index.ts
+++ libs/nextagram/shared/forms/src/index.ts
@@ -1,6 +1,6 @@
 export { Form, FormField } from '@nextagram/nextagram-shared-ui';
 
-export { FormItemWrapper } from './lib/components/form-item-wrapper';
-export { RowFormItemWrapper } from './lib/components/row-form-item-wrapper';
+export { FormItem } from './lib/components/form-item';
+export { FormItemRow } from './lib/components/form-item-row';
 
 export { useZodForm } from './lib/hooks/use-zod-form';
