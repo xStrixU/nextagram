3da95fcceba54aa63d3dcc372fcc0e9dcd5e21d5
Kacper Polak <41890821+xStrixU@users.noreply.github.com>
2025-06-05 09:18:51 +0200

    fix: fix dialog overflow (#9)

--- libs/nextagram/shared/ui/src/lib/components/custom/dialog.tsx
+++ libs/nextagram/shared/ui/src/lib/components/custom/dialog.tsx
@@ -14,10 +14,10 @@ type DialogProps = Readonly<{
 }> &
 	ComponentProps<typeof ShadcnDialog>;
 
-export const Dialog = ({ title, trigger, children }: DialogProps) => (
-	<ShadcnDialog>
+export const Dialog = ({ title, trigger, children, ...props }: DialogProps) => (
+	<ShadcnDialog {...props}>
 		<ShadcnDialogTrigger asChild>{trigger}</ShadcnDialogTrigger>
-		<ShadcnDialogContent className="space-y-4">
+		<ShadcnDialogContent className="max-h-full space-y-4 overflow-auto">
 			<ShadcnDialogHeader>
 				<ShadcnDialogTitle>{title}</ShadcnDialogTitle>
 			</ShadcnDialogHeader>
